9878f35a27406c08281f86d3ee406ad3da827052
abhay395 <140682257+abhay395@users.noreply.github.com>
2023-09-21 11:48:55 +0530

    complete

---
+++ 08miniconteext/src/components/Login.jsx
@@ -0,0 +1,29 @@
+import React,{useState,useContext} from 'react'
+import UserContext from '../context/UserContext'
+
+function Login() {
+    const handleSubmit=(e)=>{
+    e.preventDefault()
+    setUser({username,Password})
+    }
+    const [username,setusername]=useState('')
+    const [Password,setPassword]=useState('')
+    const {setUser}=useContext(UserContext)
+    
+    return (
+       <div>
+        <h2>LogeIn</h2>
+        <form action="">
+        <input type="text" value={username} onChange={(e)=>{
+            setusername(e.target.value)
+        }} placeholder='username'  />
+        <input type="text" value={Password} onChange={(e)=>{
+            setPassword(e.target.value)
+        }} placeholder='Password'  />
+        <button onClick={handleSubmit}>Submit</button>
+        </form>
+       </div> 
+    )
+}
+
+export default Login
