d30d34dac03a4ec63a198e5e6f401f073a9421a2
abhay395 <140682257+abhay395@users.noreply.github.com>
2023-09-22 13:45:49 +0530

    complete

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,16 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {Todoprovider} from './context'
+import "./App.css";
+import { useEffect } from "react";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos,setTodos]=useState([])
+  const addTodo=(todo)=>{
+    setTodos((prev)=>[{id:Date.now(),...todo},...prev])
+  }
+  const updatedTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))
 
+    
+  }
+  const deleteTodo=(id)=>{
+    setTodos((prev)=> prev.filter((todo)=>todo.id!==id))
+  }
+  const toggleComplete=(id)=>{
+    setTodos((prev)=>prev.map((prevTodo)=>prevTodo.id===id?{...prevTodo,completed:!prevTodo.completed}:prevTodo))
+    // note chek kar yaha prevTodo.id ayege aya prevTodo
+  }
+
+  useEffect(()=>{
+   const todos=JSON.parse(localStorage.getItem('todos'))
+
+   if(todos &&todos.length>0){
+    setTodos(todos)
+   }
+  },[]) 
+ 
+  useEffect(()=>{
+    localStorage.setItem('todos',JSON.stringify(todos))
+  },[todos])
   return (
-    <>
-   <h1 className='p-4 bg-cyan-300 ' >Hello</h1>
-    </>
-  )
+    <Todoprovider value={{todos,addTodo,updatedTodo,deleteTodo,toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm/></div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+           {todos.map((todo)=>(
+            <div className="w-full" key={todo.id} >
+              <TodoItem todo={todo} />
+            </div>
+           ))}
+          </div>
+        </div>
+      </div>
+    </Todoprovider>
+  );
 }
 
-export default App
+export default App;
